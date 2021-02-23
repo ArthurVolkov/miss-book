60a501c5c6916af01a09a9b6f27e3ddd8cf855e5
Arthur-Volkov <bafequan@gmail.com>
2021-02-23 21:01:20 +0200

    fixes

--- js/comps/book-search.cm.js
+++ js/comps/book-search.cm.js
@@ -1,7 +1,7 @@
 export default {
     template: `
     <section class="book-search flex justify-center">
-        <h1>search in google</h1>
+        <h2>search in google: </h2>
         <form @submit.prevent="search">
             <input v-model="bookName" type="search">
 
